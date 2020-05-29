17d6c04ec37f89fdd951d837def1a50ad6bcae92
George Chen <gchen@awakeningbyte.com>
2020-05-29 11:37:06 -0500

    fix emptyVariables test

--- graphql_test.go
+++ graphql_test.go
@@ -139,30 +139,29 @@ func TestClient_Query_errorStatusCode(t *testing.T) {
 // Test that an empty (but non-nil) variables map is
 // handled no differently than a nil variables map.
 func TestClient_Query_emptyVariables(t *testing.T) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/graphql", func(w http.ResponseWriter, req *http.Request) {
-		// body := mustRead(req.Body)
-		body := req.URL.Query()["query"][0]
-		if got, want := body, `{user{name}}`; got != want {
-			t.Errorf("got body: `%v`, want `%v`", got, want)
-		}
-		w.Header().Set("Content-Type", "application/json")
-		mustWrite(w, `{"data": {"user": {"name": "Gopher"}}}`)
-	})
-	client := graphql.NewClient("/graphql", &http.Client{Transport: localRoundTripper{handler: mux}})
+  mux := http.NewServeMux()
+  mux.HandleFunc("/graphql", func(w http.ResponseWriter, req *http.Request) {
+    body := mustRead(req.Body)
+    if got, want := body, `{"query":"{user{name}}"}`+"\n"; got != want {
+      t.Errorf("got body: %v, want %v", got, want)
+    }
+    w.Header().Set("Content-Type", "application/json")
+    mustWrite(w, `{"data": {"user": {"name": "Gopher"}}}`)
+  })
+  client := graphql.NewClient("/graphql", &http.Client{Transport: localRoundTripper{handler: mux}})
 
-	var q struct {
-		User struct {
-			Name string
-		}
-	}
-	err := client.Query(context.Background(), &q, map[string]interface{}{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	if got, want := q.User.Name, "Gopher"; got != want {
-		t.Errorf("got q.User.Name: %q, want: %q", got, want)
-	}
+  var q struct {
+    User struct {
+      Name string
+    }
+  }
+  err := client.Query(context.Background(), &q, map[string]interface{}{})
+  if err != nil {
+    t.Fatal(err)
+  }
+  if got, want := q.User.Name, "Gopher"; got != want {
+    t.Errorf("got q.User.Name: %q, want: %q", got, want)
+  }
 }
 
 // localRoundTripper is an http.RoundTripper that executes HTTP transactions
